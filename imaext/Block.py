8ded2ae232d94b76b0bcc54143403dfc1f3cbcce
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-12-23 11:17:11 +0200

    Add byte level functionality

---
+++ imaext/Block.py
@@ -0,0 +1,62 @@
+class Block:
+
+
+    """
+    A block is a set of bytes. It is used to hold a buffer of bytes in memory,
+    and the buffer in question can be either the whole image file or a 
+    subsection of it. Subsections are usually called sectors or clusters.
+    """
+
+
+    def __init__(
+        self,
+        bytes : bytearray
+    ):
+        self.bytes = bytes
+
+    
+    def get_byte(
+        self,
+        ofs : int
+    ) -> int:
+        """Returns an 8-bit unsigned integer."""
+        return self.bytes[ofs]
+
+
+    def get_word(
+        self,
+        ofs : int
+    ) -> int:
+        """Returns a 16-bit unsigned integer. The FAT12 is little-endian."""
+        return self.bytes[ofs] + self.bytes[ofs + 1] * 256
+
+
+    def get_dword(
+        self,
+        ofs : int
+    ) -> int:
+        """Returns a 32-bit unsigned integer. The FAT12 is little-endian."""
+        return (
+            self.bytes[ofs] + 
+            self.bytes[ofs + 1] * 256 +
+            self.bytes[ofs + 1] * 256 * 256 +
+            self.bytes[ofs + 1] * 256 * 256 * 256
+        )
+
+
+    def get_string(
+        self,
+        ofs : int,
+        len : int
+    ) -> str:
+        """Returns a string from the image. The length must be known."""
+        end = ofs + len
+        return self.bytes[ofs:end].decode("ascii").strip()
+
+
+    def get_block(
+        self,
+        ofs : int,
+        len : int
+    ) -> bytearray:
+        return Block(self.bytes[ofs:ofs + len])
---
+++ imaext/TestBlock.py
@@ -0,0 +1,25 @@
+import unittest
+from Block import Block
+
+class TestBlock(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open("small.bin", "br") as file:
+            cls._block = Block(bytearray(file.read()))
+
+    def test_get_byte(self):
+        self.assertEqual(self._block.get_byte(0), 0x10)
+
+    def test_get_word(self):
+        print(hex(self._block.get_word(0)))
+        self.assertEqual(self._block.get_word(0), 0x1110)
+
+    def test_get_dword(self):
+        self.assertEqual(self._block.get_dword(0), 0x13121110)
+
+    def test_get_string(self):
+        self.assertEqual(self._block.get_string(4, 8), "abcd")
+
+if __name__ == '__main__':
+    unittest.main()
